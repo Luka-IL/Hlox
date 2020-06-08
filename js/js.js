8d7d16bc97aab975d0bea7bc246bcfed746350cf
Luka-IL <63444446+Luka-IL@users.noreply.github.com>
2020-06-08 17:10:34 +0300

    Отлик элементов при скролле

--- js/js.js
+++ js/js.js
@@ -1,18 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let controller = new ScrollMagic.Controller();
     let crossTitle = $('.crossfit__title')
-    let agencyTitleTop = $('.agency__title-top')
-    let agencyTitleBottom = $('.agency__title-bottom')
+    let agencyTitleTop = $('.title-top')
+    let agencyTitleBottom = $('.title-bottom')
     let agencyVideo = $('#agency-video')
-    
-    let windowHeight = $(window).height();
-    
-    let scrollAnimation = new TimelineMax();
-    scrollAnimation.to(agencyTitleTop, 1, {xPercent:15, ease: Power4.easeOut}, '0')
-    .to(agencyVideo, 1, {y:100, ease:Linear.easeNone},'0')
-    .to(agencyTitleBottom, 1, {x:-100, ease:Linear.easeNone}, '0');
 
-    new ScrollMagic.Scene({triggerElement: '#agency-video', triggerHook:'onEnter', duration:1000})
-    .setTween(scrollAnimation)
-    .addTo(controller);
+    $('section').each(function () {
+        let scrollAnimation = new TimelineMax();
+
+        scrollAnimation.to($(this).find(agencyTitleTop), 1, {
+                x: 100,
+                ease: Power4.easeOut
+            }, '0')
+            .to($(this).find(agencyTitleBottom), 1, {
+                x: -100,
+                ease: Linear.easeNone
+            }, '0')
+            .to($(this).find('video'), 1, {
+                y: 100,
+                ease: Linear.easeNone
+            }, '0')
+            .to($(this).find('.video-sector'), 1, {
+                y: 40,
+            ease: Linear.easeNone
+            }, '0');
+
+        new ScrollMagic.Scene({
+                triggerElement: this,
+                triggerHook: 'onEnter',
+                duration: 1000
+            })
+            .setTween(scrollAnimation)
+            .addIndicators()
+            .addTo(controller);
+    });
 })
